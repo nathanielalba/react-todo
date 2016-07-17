22e664d37aa460cf859f5d0a09cc04ff81f539fb
Nathaniel Alba <nathaniel.alba@gmail.com>
2016-07-16 19:43:17 -0700

    Add filter

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -22,5 +22,30 @@ module.exports = {
     } else {
       return [];
     };
+  },
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // Filter by ShowCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+    // Filter by searchText
+
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    // Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if(!a.completed && b.completed) {
+        return -1;
+      } else if(a.completed && !b.completed) {
+        return 1;
+      };
+    });
+
+    return filteredTodos;
   }
 };
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -51,14 +51,15 @@ var TodoApp = React.createClass({
     });
   },
   render: function() {
-    var { todos } = this.state;
+    var { todos, showCompleted, searchText } = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
     return (
       <div>
         <div className="row">
           <div className="columns medium-6 large-4 small-centered">
             <TodoSearch onSearch={this.handleSearch} />
-            <TodoList todos={todos} onToggle={this.handleToggle} />
+            <TodoList todos={filteredTodos} onToggle={this.handleToggle} />
             <AddTodo handleAddTodo={this.handleAddTodo} />
           </div>
         </div>
