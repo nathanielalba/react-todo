94a424aab7cc493f2637cb50ef9d66893ecfeafe
Nathaniel Alba <nathaniel.alba@gmail.com>
2016-07-21 01:27:50 -0700

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -31,4 +31,38 @@ describe('reducers', () => {
       expect(res).toBe(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Shit'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toBe(1);
+      expect(res[0].todo).toEqual(action.todo);
+    });
+
+    it('should toggle a todo', () => {
+      var todos = [{
+        id: 1,
+        text: 'Hello there',
+        completed: false,
+        createdAt: 'now',
+        completedAt: undefined
+      }];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+
+
+      var res = reducers.todosReducer(df(todos), df(action));
+      expect(res[0].completed).toBe(true);
+      expect(res[0].completedAt).toBeA('number');
+    });
+  });
 });
